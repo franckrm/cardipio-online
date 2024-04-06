43a14965586afd4f4b8d2bba7d574684c071c3cf
Ribeirovisk <franckrm2001@gmail.com>
2024-04-05 23:22:57 -0300

    feat: add function to send the order by message via whatsapp to the restaurant

--- js/app.js
+++ js/app.js
@@ -8,8 +8,9 @@ var MEU_CARRINHO = [];
 var MEU_ENDERECO = null;
 
 var VALOR_CARRINHO = 0;
-
 var VALOR_ENTREGA = 5;
+
+var CELULAR_EMPRESA = '5521968629008';
 
 cardapio.eventos = {
  init: () =>{
@@ -423,8 +424,37 @@ cardapio.metodos = {
 
         });
 
-        $('#resumoEndereco').html(`${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`)
-        $('#cidadeEndereco').html(`${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`)
+        $('#resumoEndereco').html(`${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`);
+        $('#cidadeEndereco').html(`${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`);
+
+        cardapio.metodos.finalizarPedido();
+    },
+
+    //Atualiza o link do botão do Whatsapp    
+    finalizarPedido: ()=>{
+        if(MEU_CARRINHO.length > 0 && MEU_ENDERECO != null){
+            var texto = 'Olá gostaria de fazer um pedido:';
+            texto += `\n*Itens do pedido: *\n\n\${itens}`; 
+            texto +=`\n*Endereço de entrega:*`;
+            texto += `\n${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`;
+            texto += `\n${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`;
+            texto += `\n\n*Total (com entrega): R$ ${(VALOR_CARRINHO+VALOR_ENTREGA).toFixed(2).replace('.', ',')}*`;
+
+            var itens = '';
+
+            $.each(MEU_CARRINHO, (i,e)=>{
+                itens += `*${e.qntd}x* ${e.name} ...... R$ ${e.price.toFixed(2).replace('.', ',')} \n`;
+
+                if((i+1)== MEU_CARRINHO.length){
+                    texto = texto.replace(/\${itens}/g, itens);
+                    //converter a url
+                    let encode = encodeURI(texto);
+                    let URL = `https://wa.me/${CELULAR_EMPRESA}?text=${encode}`;
+
+                    $('#btnEtapaResumo').attr('href', URL);
+                }
+            })
+        }
     },
 
     mensagem: (texto, cor='red', tempo=3500)=>{
